24bec3ef65cca8ee0a4710fc3848c08b63a03a05
Alexander <alexschalk@gmail.com>
2024-04-22 15:32:33 -0400

    Module 5 Djangogirls

---
+++ M5_Programming/my_sum/__init__.py
@@ -0,0 +1,11 @@
+'''Alexander Schalk
+SDEV 220 - Evan Mitchell
+M5 Programming Assignment
+04/22/2024
+'''
+
+def sum(arg): #Returns sum of iter
+    total = 0
+    for val in arg:
+        total += val
+    return total
